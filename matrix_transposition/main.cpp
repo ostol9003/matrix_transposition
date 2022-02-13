f8f32f6b3f309bcd980e09c5560fbfee580301c4
OstolskiM <ostolski.marcin@gmail.com>
2022-02-13 16:18:33 +0100

    Created

--- matrix_transposition/main.cpp
+++ matrix_transposition/main.cpp
@@ -6,9 +6,49 @@
 //
 
 #include <iostream>
+using namespace std;
 
+void welcome()
+{
+    cout<<"\t\tHellow!\nI will transpose your matrix!\n\n\n";
+};
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int n, m;
+    
+    welcome();
+    
+    cout<<"Enter quantity of lines: ";
+    cin>>n;
+    
+    cout<<"\nEnter quantity of columns: ";
+    cin>>m;
+    
+    int tab[n][m];
+    
+    cout<<"\nEnter your matrix: \n";
+    
+    for (int i=0;i<n;i++)
+        for (int y=0;y<m;y++)
+            cin>>tab[i][y];
+    
+    cout<<"\nYour Matrix:\n";
+    
+    for (int i=0;i<n;i++)
+    {
+        for (int y=0;y<m;y++)
+            cout<<tab[i][y]<<"\t";
+        cout<<"\n";
+    }
+    
+    cout<<"\nTransposed matrix: \n";
+    
+    for (int i=0;i<m;i++)
+    {
+        for (int y=0;y<n;y++)
+            cout<<tab[y][i]<<"\t";
+        cout<<"\n";
+    }
+    
+    
     return 0;
 }
